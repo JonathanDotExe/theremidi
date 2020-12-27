4c0cf66fb20e36fb0d9ec9fc6465e282f04086b9
DatDonathan <jojo08082003@gmail.com>
2020-12-27 21:52:39 +0100

    Added testing midi code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # theremidi
 Software for a MIDI Theremin built using an ESP32
+
+# Used libraries
+* MIDIUSB Arduino library licensed under LGPL (https://github.com/arduino-libraries/MIDIUSB)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
+#include "MIDIUSB.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.write("Test");
+  byte channel = 0;
+  MidiUSB.sendMIDI({0x09, 0x90 | channel, 60, 127}); //Note on
+  delay(500);
+  MidiUSB.sendMIDI({0x08, 0x80 | channel, 60, 127}); //Note off
+  delay(500);
 }
