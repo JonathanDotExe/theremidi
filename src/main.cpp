8c419371ff41c7704211775c03715a7fb9ae048e
DatDonathan <jojo08082003@gmail.com>
2021-01-09 21:35:07 +0100

    Changed firmware ot arduino uno

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
-#include "MIDIUSB.h"
+#include "MidiUSB.h"
 
 void setup() {
-  
+  Serial.begin(31250);
+}
+
+void noteOn(byte channel, byte note, byte velocity) {
+  midiEventPacket_t event = {0x09, 0x90 | channel, note, velocity};
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
+}
+
+void noteOff(byte channel, byte note, byte velocity) {
+  midiEventPacket_t event = {0x08, 0x80 | channel, note, velocity};
+  MidiUSB.sendMIDI(event);
+  MidiUSB.flush();
 }
 
 void loop() {
   byte channel = 0;
-  MidiUSB.sendMIDI({0x09, 0x90 | channel, 60, 127}); //Note on
+  noteOn(channel, 60, 127); //Note on
   delay(500);
-  MidiUSB.sendMIDI({0x08, 0x80 | channel, 60, 127}); //Note off
+  noteOff(channel, 60, 127); //Note on
   delay(500);
 }
