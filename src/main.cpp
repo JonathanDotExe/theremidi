102ab131e5090ebf429e84cac74ef6f89bd0b1df
DatDonathan <jojo08082003@gmail.com>
2021-03-30 12:54:12 +0200

    Added light sensor library

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,10 @@
 #include <Arduino.h>
-#include "MidiUSB.h"
+#include "Adafruit_VL53L0X.h"
 
 void setup() {
-  Serial.begin(31250);
-}
-
-void noteOn(byte channel, byte note, byte velocity) {
-  midiEventPacket_t event = {0x09, 0x90 | channel, note, velocity};
-  MidiUSB.sendMIDI(event);
-  MidiUSB.flush();
-}
-
-void noteOff(byte channel, byte note, byte velocity) {
-  midiEventPacket_t event = {0x08, 0x80 | channel, note, velocity};
-  MidiUSB.sendMIDI(event);
-  MidiUSB.flush();
+  Serial.begin(115200);
 }
 
 void loop() {
-  byte channel = 0;
-  noteOn(channel, 60, 127); //Note on
-  delay(500);
-  noteOff(channel, 60, 127); //Note on
-  delay(500);
+  Serial.println("Hallo");
 }
