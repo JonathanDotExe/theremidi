acb1147e65c93fafda99d0667211379afeba5842
DatDonathan <jojo08082003@gmail.com>
2021-04-10 10:17:49 +0200

    Added demo measure ment program

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 #include "Adafruit_VL53L0X.h"
 
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
 void setup() {
   Serial.begin(115200);
+
+  //Wait for serial
+  while (! Serial) {
+    delay(1);
+  }
+
+  if (!lox.begin()) {
+    Serial.println("Failed to initialize sensor");
+    while (1); //Sleep foreever
+  }
 }
 
 void loop() {
-  Serial.println("Hallo");
+  VL53L0X_RangingMeasurementData_t data;
+  lox.rangingTest(&data, false);
+
+  if (data.RangeStatus != 4) {
+    Serial.println(data.RangeMilliMeter);
+  }
+  else {
+    Serial.println("out of range");
+  }
 }
