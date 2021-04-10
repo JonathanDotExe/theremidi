ac4404b4ff18bffe2a7db9093c6d52cd159051e1
DatDonathan <jojo08082003@gmail.com>
2021-04-10 10:45:25 +0200

    Added MIDI util functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,31 @@
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
+struct CalibrationData {
+  uint16_t min_dst = 30;
+  uint16_t max_dst = 300;
+};
+
+void message(int operation, int channel, uint8_t first_data, uint8_t second_data) {
+  uint8_t cmd = (operation & 0xF0) | (channel & 0x0F);
+  Serial.write(cmd);
+  Serial.write(first_data & 0x7F);
+  Serial.write(second_data & 0x7F);
+}
+
+void note_on(int channel, int note, int velocity) {
+  message(0x90, channel, note, velocity);
+}
+
+void note_off(int channel, int note) {
+  message(0x80, channel, note, 0);
+}
+
+void cc(int channel, int cc, double value) {
+  message(0x90, channel, cc, value * 127);
+}
+
+
 void setup() {
   Serial.begin(115200);
 
